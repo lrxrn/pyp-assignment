1eca9e8f4cc9ac9701aaf4ab73a274373c01dd4a
Mazin <TP081779@mail.apu.edu.my>
2024-08-24 00:55:26 +0800

    fix nameerror

--- Python/Individual/mazin/main.py
+++ Python/Individual/mazin/main.py
@@ -171,21 +171,25 @@ def edit_customer():
 
 def delete_customer():
     print("-" * 50)
-    global user_nm
-    user = input("Enter username to delete: ").lower()
-    customer_file = open("customer_list", "r")
-    customer_file = list(customer_file)
-    for line in customer_file:
-        if user in line.split(", ")[0]:
-            user_nm = line
-            del customer_file[customer_file.index(line)]
+    while True:
+        global user_nm
+        user = input("Enter username to delete: ").lower()
+        customer_file = open("customer_list", "r")
+        customer_file = list(customer_file)
+        for line in customer_file:
+            if user == line.split(", ")[0]:
+                user_nm = line
+                del customer_file[customer_file.index(line)]
 
-    with open("customer_list", "w") as f:
-        for customer in customer_file:
-            f.write(f"{customer}")
+        with open("customer_list", "w") as f:
+            for customer in customer_file:
+                f.write(f"{customer}")
 
-    print(f"Deleted user: {user_nm}")
-    manage_customer()
+        try:
+            print(f"Deleted user: {user_nm.split(', ')[0]}")
+            manage_customer()
+        except NameError:
+            print("User not found")
 
 
 def manage_customer():
