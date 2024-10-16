42427d78974552417761c8b6855167cd0db07ccb
SoloNexus <solonexus365@gmail.com>
2024-10-16 22:33:33 +0800

    Update Chef.py

--- Roles/Chef/Chef.py
+++ Roles/Chef/Chef.py
@@ -1,255 +1,249 @@
 import json
 from datetime import datetime
 from tabulate import tabulate
-from math import ceil
+TABLE_FORMAT = "rounded_outline"  # The table format to be displayed
 
-TABLE_FORMAT = "rounded_outline"  # Table format to display in tabular form
-ORDERS_FILE = "Orders.json"
-REQUESTS_FILE = "Ingredients.json"
+# Import common functions from functions.py
+from Modules.utils import inp, printD, get_next_id
 
+# 0 - Start function
+def start(cur_usr):
+    while True:
+        printD(f"Chef menu", "magenta", True)
+        print("1. View orders placed by customers. \n2. Update orders \n3. Request ingredients \n4. Update own profile. \n5. Logout")
 
-# Function to get the current date and time in the formats 05-Sep-2024 and 10:30 PM respectively
-def time_object():
-    now = datetime.now()
-    return now.strftime('%d-%b-%Y'), now.strftime('%I:%M %p')
+        option = inp("Choose an option from 1 to 5: ", "int", [1, 2, 3, 4, 5])
+        match option:
+            case 1:
+                show_orders(cur_usr)
+            case 2:
+                update_order_status(cur_usr)
+            case 3:
+                request_ingredients(cur_usr)
+            case 4:
+                update_profile(cur_usr, start)
+            case 5:
+                logout(cur_usr)
 
-
-# Function to retrieve active orders from the database (orders_object)
-def get_orders(orders_object):
-    # returning an order if status is not "Completed" or "Delivered"
-    return [
-        {"order_id": order, "items": orders_object[order]["details"]["items"]}
-        for order in orders_object if orders_object[order]["status"] not in ["Completed", "Delivered"]
-    ]
-
-
-# Function to display orders in a table format using tabulate.
-# Returns a boolean value to be used by the update_order_status() function
-def show_orders(orders_object):
-    orders = get_orders(orders_object)
-    if not orders:  # Check if requests are made or not
+# 1 - Display orders in a grid format
+def show_orders(current_user):
+    orders = get_orders()
+    if not orders:  # Checking the truthy value of orders to make sure it's not empty
         print("No orders :D")
         return False
 
     headers = ["Order ID", "Item ID", "Quantity"]
     rows = []
 
-    # Building row data for table display
+    # Preparing row data
     for order in orders:
+        # Add the first item with the order ID
         for index, item in enumerate(order["items"]):
             if index == 0:
                 rows.append([order['order_id'], item['ID'], item['quantity']])
             else:
+                # For subsequent items, leave the order ID blank
                 rows.append(["", item['ID'], item['quantity']])
-        rows.append(["-----------", "----------", "---------"])  # Separator between orders
-    rows.pop()  # Remove the last separator
+        rows.append(["-----------", "----------", "---------"])  # Creates a separator between orders
+    rows.pop()  # removing last separator for the final order
 
     print(tabulate(rows, headers, tablefmt=TABLE_FORMAT))
     return True
 
+# 1.0.1 - Function to get the current date and time in the formats 05-Sep-2024 and 10:30 PM respectively
+def time_object():
+    now = datetime.now()
+    return now.strftime('%d-%b-%Y'), now.strftime('%I:%M %p')
 
-# Function to update the status of an order to either "Completed" or "Delivered"
-def update_order_status(orders_object):
-    active_orders = {key: value for (key, value) in orders_object.items() if not
-    (value["status"] == "Delivered" or value["status"] == "Completed")}
+# 1.0.2 - Function to retrieve active orders from the database
+def get_orders():
     try:
-        # Checking the boolean value returned by show_orders() function to
-        if show_orders(active_orders):
-            order_id = input("\nPlease input the order ID to mark as completed\nOrder ID: ").upper().strip()
-            if order_id not in active_orders:
-                print("Invalid Order ID")
-                raise ValueError
-            else:
-                dining_option = orders_object[order_id]["details"]["diningOption"]
-                orders_object[order_id]["status"] = "Completed" if dining_option == "Dine-in" else "Delivered"
-        return orders_object
-    except ValueError:
-        return update_order_status(orders_object)
+        with open("Data/orders.json", "r") as file:
+            orders_object = json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"Error reading JSON file: {e}")
+        return {}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
+    return [
+        {"order_id": order, "items": orders_object[order]["details"]["items"]}
+        for order in orders_object if orders_object[order]["status"] not in ["Completed", "Delivered"]
+    ]
 
+# 1.0.3 - Function to save orders to the database
+def save_orders(orders):
+    with open("Data/orders.json", "w") as file:
+        json.dump(orders, file, indent=4)
 
-# Function to display ingredients in a tabular format
-def show_ingredients(ingredients):
-    headers = ["Item ID", "Ingredient", "Measure (Unit)"]
-    rows = [[i + 1, ingredient, details["Measure"]] for i, (ingredient, details) in enumerate(ingredients.items())]
-    print(tabulate(rows, headers, tablefmt=TABLE_FORMAT))
+# 2 - Update order status to "Completed" or "Delivered" depending on dining option and cannot be undone.
+def update_order_status(current_user):
+    if show_orders(current_user):
+        orders_object = get_orders_raw()
+        order_id = input("\nPlease input the order ID to mark as completed\nOrder ID: ").upper()
+        if order_id not in orders_object:
+            print("Invalid Order ID")
+            return update_order_status(current_user)
+        else:
+            dining_option = orders_object[order_id]["details"]["diningOption"]
+            orders_object[order_id]["status"] = "Completed" if dining_option == "Dine-in" else "Delivered"
+            save_orders(orders_object)
 
+# 2.0.1 - Function to retrieve all orders from the database 
+def get_orders_raw():
+    try:
+        with open("Data/orders.json", "r") as file:
+            orders_object = json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"Error reading JSON file: {e}")
+        return {}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
+    return orders_object
 
-# Function to display ingredient requests in a tabular format
-def show_requests(requests):
-    if any(v["Quantity"] > 0 for v in requests.values()):
-        headers = ["Item ID", "Ingredient", "Quantity", "Measure (Unit)"]
-        rows = [[i + 1, ingredient, details["Quantity"], details["Measure"]] for i, (ingredient, details) in
-                enumerate(requests.items()) if details["Quantity"] > 0]
-        print(tabulate(rows, headers, tablefmt=TABLE_FORMAT))
-    else:
-        print("\nNo requests made :D")
+# 3 - Request ingredients and update the quantity
+def request_ingredients(current_user):
+    requested_items = list()  # A set to keep track of requests made by the user
+    handle_request_options(current_user, requested_items)
 
-
-# Function to request ingredients, handling inputs and errors
-# Returns a request object containing the requests made by the chef
-def request_ingredients():
-    requested_ingredients = dict()  # Store the requested ingredients
-    while True:
-        try:
-            ingredient = input("\nPlease input the ingredient you want to request\nIngredient: ")
-            if not ingredient.replace(" ", "").isalpha():
-                raise ValueError("Invalid ingredient input. Please enter a valid name.")
-        except ValueError as e:
-            print(f"{e}")
-            continue
-        try:
-            quantity = ceil(float(input("Quantity: ")))
-            if quantity <= 0:
-                raise ValueError("Quantity must be greater than zero.")
-        except ValueError as e:
-            print(f"Invalid quantity: {e}")
-            continue
-        try:
-            measure = input("Measure: ")
-            valid_measures = ["Kg", "L", "Litres", "Bottle", "Bottles", "Can", "Cans", "Tray", "Trays"]
-            if measure.capitalize() not in valid_measures:
-                raise ValueError("Invalid measure.")
-        except ValueError as e:
-            print(f"Invalid measure: {e}")
-            continue
-
-        # Adding the request to the requests dictionary
-        requested_ingredients.update(get_ingredients(ingredient, quantity, measure))
-
-        if not handle_request_options(requested_ingredients):
-            break
-
-    # Returning the requested items and making sure the quantity is greater than 0
-    return {k: v for k, v in requested_ingredients.items() if v["Quantity"] > 0}
-
-
-# Function to handle request options such as adding, editing, or deleting ingredients
-# Returns a boolean value used to determine whether to break out of the outer loop in request_ingredients()
-def handle_request_options(requested_ingredients):
+# 3.1 - Handle request options for adding/editing/deleting ingredients
+def handle_request_options(current_user, requested_ingredients: list):
     while True:
         show_requests(requested_ingredients)
         try:
             option = int(
                 input("\n1. Add ingredient\n2. Edit ingredient\n3. Delete ingredient\n4. Complete request\nOption: "))
             if option not in [1, 2, 3, 4]:
-                raise ValueError("Invalid option.")
-        except ValueError as e:
-            print(f"{e}")
+                raise ValueError
+        except ValueError:
+            print("\nInvalid option")
             continue
 
         if option == 1:
-            return True
+            add_ingredient(requested_ingredients, current_user)
         elif option == 2:
-            edit_request(requested_ingredients)
+            edit_request(requested_ingredients, current_user)
         elif option == 3:
-            delete_request(requested_ingredients)
+            delete_request(requested_ingredients, current_user)
         elif option == 4:
-            return False
+            complete_request(requested_ingredients, current_user)
 
+# 3.1.1 - Add a new ingredient to the request
+def add_ingredient(requested_ingredients: list, current_user):
+    item_name = input("Enter the name of the ingredient you'd like to request: ")
+    item_unit = input("Unit of measure: ")
+    try:
+        item_quantity = int(input("Quantity number: "))
+        if item_quantity < 1:
+            raise ValueError
+    except ValueError:
+        print("\nInvalid quantity")
+        add_ingredient(requested_ingredients)
+        return
 
-# Function to edit an existing request
-def edit_request(requested_ingredients):
+    requested_ingredient = {
+        "name": item_name,
+        "quantity": item_quantity,
+        "unit": item_unit
+    }
+
+    requested_ingredients.append(requested_ingredient)
+    print(f"Added {item_name} to the request")
+    handle_request_options(current_user, requested_ingredients)
+
+# 3.1.2 - Edit an existing request
+def edit_request(requested_ingredients: list, current_user):
     show_requests(requested_ingredients)
 
     try:
         item_number = int(input("\nChoose an ingredient to edit: "))
-        if item_number not in range(1, len(requested_ingredients) + 1):
-            raise ValueError("Invalid Item ID.")
-    except ValueError as e:
-        print(f"{e}")
+        if item_number > len(requested_ingredients) or item_number < 1:
+            raise ValueError
+    except ValueError:
+        print("\nInvalid Item ID")
+        edit_request(requested_ingredients)
         return
 
+    item_name = requested_ingredients[item_number - 1]["name"]
     try:
         new_quantity = int(input("New Quantity: "))
         if new_quantity < 1:
-            raise ValueError("Quantity must be greater than zero.")
-    except ValueError as e:
-        print(f"{e}")
+            raise ValueError
+    except ValueError:
+        print("\nInvalid quantity")
+        edit_request(requested_ingredients)
         return
 
-    item_name = list(requested_ingredients.keys())[item_number - 1]
-    requested_ingredients[item_name]["Quantity"] = new_quantity
+    print(f"Changed quantity of {item_name} from {requested_ingredients[item_number - 1]['quantity']} to {new_quantity}")
 
+    requested_ingredients[item_number - 1]["quantity"] = new_quantity
+    handle_request_options(current_user, requested_ingredients)
 
-# Function to delete an existing request
-def delete_request(requested_ingredients):
+# 3.1.3 - Delete an existing request
+def delete_request(requested_ingredients: list, current_user):
     show_requests(requested_ingredients)
 
     try:
         item_number = int(input("Choose the ingredient to delete: "))
-        if item_number not in range(1, len(requested_ingredients) + 1):
-            raise ValueError("Invalid Item ID.")
-    except ValueError as e:
-        print(f"{e}")
+        if item_number > len(requested_ingredients) or item_number < 1:
+            raise ValueError
+    except ValueError:
+        print("\nInvalid Item ID")
+        delete_request(requested_ingredients)
         return
 
-    item_name = list(requested_ingredients.keys())[item_number - 1]
-    requested_ingredients.pop(item_name)
+    item_name = requested_ingredients[item_number - 1]["name"]
+    print(f"Deleted {item_name} from the request")
+    requested_ingredients.pop(item_number - 1)
+    handle_request_options(current_user, requested_ingredients)
 
-
-# Function to complete the ingredient request and create a boilerplate object
-def complete_request(request_object, request_id, name):
+# 3.1.4 - Complete the request by creating a boilerplate
+def complete_request(requested_items, current_user):
+    request_id = get_next_id("ingredients")
     date, time = time_object()
-    boiler_plate = {
-        f"{request_id}": {
+    request_object = {
             "status": "pending",
-            "items": [{"name": req, "quantity": details["Quantity"], "unit": details["Measure"]}
-                      for req, details in request_object.items()],
-            "request_Chef": {"user": name, "date": date, "time": time},
+            "items": requested_items,
+            "request_chef": {"user": current_user, "date": date, "time": time},
             "review_user": {"user": "", "date": "", "time": ""}
         }
-    }
-    # print(boiler_plate)
-    print("\nRequest successfully submitted :D")
-    return boiler_plate
+    add_request_to_file(request_id, request_object)
+    print(f"\nRequest with id {request_id} was successfully submitted :D")
+    start(current_user)
 
+# 3.2 - Display ingredient requests
+def show_requests(requests):
+    if len(requests) != 0:
+        headers = ["#", "Ingredient", "Quantity", "Measure (Unit)"]
+        rows = [[i + 1, ingredient["name"], ingredient["quantity"], ingredient["unit"]] for i, (ingredient) in enumerate(requests)]
+        print(tabulate(rows, headers, tablefmt=TABLE_FORMAT))
+    else:
+        print("\nNo requests made :D")
 
-# Function to create a dictionary for requested ingredients
-def get_ingredients(Ingredient, Quantity, Measure):
-    return {
-        Ingredient.capitalize(): {
-            "Quantity": ceil(Quantity),
-            "Measure": Measure.capitalize()
-        }
-    }
-
-
-# Main function to run the program
-def main():
-    def load_file(file_name):
-        with open(file_name) as file:
-            return json.load(file)
-
-    def write_to_file(file_name, data, mode="w"):
-        with open(file_name, mode) as file:
-            json.dump(data, file, indent=4)
-        print(f"{file_name} updated successfully")
-
-    def _show_orders_():
-        ORDERS = load_file(ORDERS_FILE)
-        show_orders(ORDERS)
-
-    def _update_order_status_():
-        ORDERS = load_file(ORDERS_FILE)
-        orders = update_order_status(ORDERS)
-        ORDERS.update(orders)
-        write_to_file(ORDERS_FILE, orders)
-
-    def make_request(request_id, chef_name):
-        REQUESTS = load_file(REQUESTS_FILE)
-        request = complete_request(request_ingredients(), request_id, chef_name)
-        REQUESTS.update(request)
-        write_to_file(REQUESTS_FILE, REQUESTS)
-
+# 3.3 - Add a complete request to the file
+def add_request_to_file(request_id, request_object):
     try:
-        # _show_orders_()
-        _update_order_status_()
-        # make_request("ING-011", "Feynman")
+        with open("Data/Ingredients.json", "r") as file:
+            requests = json.load(file)
     except json.JSONDecodeError as e:
         print(f"Error reading JSON file: {e}")
+        return
     except Exception as e:
         print(f"Error: {e}")
+        return
 
+    requests[request_id] = request_object
 
-if __name__ == "__main__":
-    main()
+    with open("Data/Ingredients.json", "w") as file:
+        json.dump(requests, file, indent=4)
+
+# 4 - Update profile function
+def update_profile(cur_usr, return_func):
+    from main import update_profile as update_profile_main
+    update_profile_main(cur_usr, return_func)
+
+# 5 - Logout function
+def logout(cur_usr):
+    from main import logout as logout_main
+    logout_main(cur_usr)
