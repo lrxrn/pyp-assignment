99f3968ead5cafdd36de10e52ddc21e4ee2097f8
Meesum Ahmed <49319669+lrxrn@users.noreply.github.com>
2024-11-03 21:19:30 +0800

    remoVe test

--- test.py
+++
@@ -1,6 +0,0 @@
-from Modules.utils import display_table
-from Modules.db import db_getKey
-
-user = db_getKey("users", "ibaan")
-data = [["Sun",696000,1989100000],["Earth",6371,5973.6],["Moon",1737,73.5],["Mars",3390,641.85]]
-display_table(data=data, tablefmt="plain")
